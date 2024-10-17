32a899288ba001fd5632207521d2a638677c14e5
Pedro <105327849+pedro-brandt@users.noreply.github.com>
2024-10-17 15:05:01 -0300

    Add files via upload

---
+++ Aplica-oReactApp-main/Aplica-oReactApp-main/src/App.js
@@ -0,0 +1,34 @@
+import "../src/App.css"
+
+function App() {
+  return (
+    <div className="App-pai">
+
+<div className="menu-container">
+  
+     <div className="menu">
+     <p>
+      Menu
+     </p>
+     <div className="bts">
+     <button id="bt3">Login</button>
+     <button id="bt4">Cadastro</button>
+     </div>
+     </div>
+     </div>
+  
+     <h1> Bem vindo ao meu site!</h1>
+
+     <div className="App">
+      <h3>Site</h3>
+       <p>
+       Testo do site</p>
+
+     </div>
+    
+     
+    </div>
+  );
+}
+
+export default App;
